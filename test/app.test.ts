cf6d6606526ed488c6d5af7f030b80779b9bb81f
Roberto Fasciolo <roberto.fasciolo@surveypal.com>
2023-04-21 15:57:01 +0300

    Reimplemented without supertest library

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,11 +1,30 @@
-import { describe, expect, test } from "@jest/globals"
-import request from "supertest"
+import axios from "axios"
+import { describe, expect, test, beforeAll, afterAll } from "@jest/globals"
+import type { Server } from "http"
+import type { AddressInfo } from "net"
 
 import app from "../src/app"
 
 describe("app tests", () => {
+  let server: Server;
+  let baseUrl: string;
+
+  beforeAll(() => {
+    server = app.listen(0);
+    const port = (server.address() as AddressInfo).port;
+    console.log('App listening on port', port);
+    baseUrl = `http://localhost:${port}`
+  });
+  afterAll(() => {
+    server.close()
+  });
   test("/oauth/v1/token POST", async () => {
-    const response = await request(app).post("/oauth/v1/token")
-    expect(response.statusCode).toBe(200)
+    const response = await axios({
+      method: 'post',
+      url: `${baseUrl}/oauth/v1/token`,
+    })
+
+    console.log(response)
+    expect(response.status).toBe(200)
   })
 })
