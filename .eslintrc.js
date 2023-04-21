e2c1ed321a21c0803375fcf3046b2855ae4077be
Roberto Fasciolo <roberto.fasciolo@surveypal.com>
2023-04-21 15:00:04 +0300

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  rules: {
+  }
+}
