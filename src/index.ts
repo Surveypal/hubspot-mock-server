37f790e3fc83f297882ca71b75fbd9b44919b7c1
Roberto Fasciolo <roberto.fasciolo@surveypal.com>
2023-05-05 09:57:31 +0300

    feat: Added access logs to express server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import app from "./app"
 
 const port = process.env.PORT ?? 8000
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const morgan = require("morgan")
+app.use(morgan("combined"))
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
 })
